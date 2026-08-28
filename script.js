f884c773bc3bf40c1d63c23edefae4fa190f4502
rafaelymxv <rafaely.rosa.pires@escola.pr.gov.br>
2026-08-28 09:00:05 -0300

    ok

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Alternador de Abas (Clássico / Shippuden)
+// Função para controlar a troca de Abas (Clássico / Shippuden)
 function openTab(tabName) {
     const contents = document.querySelectorAll('.tab-content');
     const buttons = document.querySelectorAll('.tab-btn');
@@ -10,7 +10,7 @@ function openTab(tabName) {
     event.currentTarget.classList.add('active');
 }
 
-// Alternador de Temas (Modo Folha / Modo Akatsuki)
+// Função do Botão de Alternar o Tema (Folha / Akatsuki)
 const btnTema = document.getElementById('btn-tema');
 btnTema.addEventListener('click', () => {
     document.body.classList.toggle('tema-akatsuki');
@@ -22,12 +22,15 @@ btnTema.addEventListener('click', () => {
     }
 });
 
-// Lógica do Quiz Ninja
+// Função para o Quiz Ninja
 function responder(correto) {
     const resultado = document.getElementById('quiz-resultado');
     if (correto) {
-        resultado.textContent = " Resposta Certa! Você demonstrou ter o nível de um Hokage!";
+        resultado.textContent = " Resposta Certa! Você demonstrou conhecimento de nível Hokage!";
     } else {
         resultado.textContent = " Resposta Errada! Volte para a Academia Ninja e tente novamente!";
     }
 }
+
+
+
